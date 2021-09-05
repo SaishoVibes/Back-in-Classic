5299b7d3af19d5bc358393e62d8033901f3acd9c
Mantis <Brickbuilder876@gmail.com>
2021-09-05 21:29:14 +0100

    Back In Classic 0.5.3 PRE

--- src/main/java/net/backinclassic/BackInClassicMod.java
+++ src/main/java/net/backinclassic/BackInClassicMod.java
@@ -39,13 +39,22 @@ import net.minecraft.block.Block;
 
 import java.util.function.Supplier;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+
 @Mod("back_in_classic")
+// @Mod(value = BackInClassic.MODID)
 public class BackInClassicMod {
 	public static final Logger LOGGER = LogManager.getLogger(BackInClassicMod.class);
 	private static final String PROTOCOL_VERSION = "1";
 	public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(new ResourceLocation("back_in_classic", "back_in_classic"),
 			() -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
-	public BackInClassicModElements elements;
+    public static final String MODID = "back_in_classic";
+	public static final String NAME = "Back in Classic";
+	public static final String VERSION = "0.5.2";
+    public BackInClassicModElements elements;
 	public BackInClassicMod() {
 		elements = new BackInClassicModElements();
 		FMLJavaModLoadingContext.get().getModEventBus().register(this);
