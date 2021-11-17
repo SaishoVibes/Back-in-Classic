7c32a6995357143581fe5e49d4df15c88cb91e98
Mantis <Brickbuilder876@gmail.com>
2021-11-17 21:27:29 +0000

    push origin

--- src/main/java/net/backinclassic/BackInClassicMod.java
+++ src/main/java/net/backinclassic/BackInClassicMod.java
@@ -53,7 +53,7 @@ public class BackInClassicMod {
 			() -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
     public static final String MODID = "back_in_classic";
 	public static final String NAME = "Back in Classic";
-	public static final String VERSION = "0.5.4-beta.5";
+	public static final String VERSION = "0.5.4-beta.7";
     public BackInClassicModElements elements;
 	public BackInClassicMod() {
 		elements = new BackInClassicModElements();
